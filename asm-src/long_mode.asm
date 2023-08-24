6ed8230ca25c76b9f528d66c1e67e0dbae9d01b1
Vickerinox <05viktorinox@gmail.com>
2023-08-24 22:46:47 +0200

    added target to gitignore

--- asm-src/long_mode.asm
+++ asm-src/long_mode.asm
@@ -1,5 +1,6 @@
 global long_mode_start
 
+
 section .text
 bits 64
 long_mode_start:
@@ -10,6 +11,8 @@ long_mode_start:
     mov ds, ax
     mov es, ax
 
+    extern rust_start
+    call rust_start
     
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
