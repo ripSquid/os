3c256deece3a27c19828b73d1516ff3c71280730
Vickerinox <05viktorinox@gmail.com>
2023-08-27 12:40:01 +0200

    helped macros

--- os/src/display/macros.rs
+++ os/src/display/macros.rs
@@ -2,8 +2,9 @@
 
 macro_rules! print_str {
     ($term: expr) => {
+        use crate::{display::primitives::PrimitiveDisplay as Npd, DefaultVgaBuffer as Vpb};
         let mut writer = crate::display::DefaultVgaWriter::new(unsafe {
-            &mut *(0xB8000 as *mut DefaultVgaBuffer)
+            &mut *(0xB8000 as *mut Vpb)
         });
         writer.prepare_print();
         writer.write_str($term);
@@ -14,8 +15,9 @@ pub(crate) use print_str;
 
 macro_rules! print_hex {
     ($term: expr) => {
+        use crate::{display::primitives::PrimitiveDisplay as Hpd, DefaultVgaBuffer as Hpb};
         let mut writer = crate::display::DefaultVgaWriter::new(unsafe {
-            &mut *(0xB8000 as *mut DefaultVgaBuffer)
+            &mut *(0xB8000 as *mut Hpb)
         });
         writer.prepare_print();
         writer.write_bytes($term.as_hexadecimal_ascii().as_ref());
@@ -26,8 +28,9 @@ pub(crate) use print_hex;
 
 macro_rules! print_num {
     ($term: expr) => {
+        use crate::DefaultVgaBuffer as StrVb;
         let mut writer = crate::display::DefaultVgaWriter::new(unsafe {
-            &mut *(0xB8000 as *mut DefaultVgaBuffer)
+            &mut *(0xB8000 as *mut StrVb)
         });
         writer.prepare_print();
         writer.write_bytes($term.as_numeric_ascii().as_ref());
