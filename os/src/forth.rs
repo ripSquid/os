04e88b64710bae48b585d5f9af50da2cf3718f1e
ripSquid <44409107+ripSquid@users.noreply.github.com>
2023-11-17 17:55:38 +0100

    yer

--- os/src/forth.rs
+++ os/src/forth.rs
@@ -1,24 +1,32 @@
 use alloc::boxed::Box;
+use alloc::fmt::format;
 use alloc::string::ToString;
+use alloc::sync::Arc;
 use alloc::{collections::BTreeMap, format, string::String, vec::Vec};
 use alloc::vec;
 
+use base::display::DefaultVgaWriter;
 use fs::{LittleManApp, InstallableApp, Path, AppConstructor, StartError, OsHandle};
+use spin::rwlock::RwLock;
 use crate::display::{STATIC_VGA_WRITER, BitmapVgaWriter, VgaPalette, VgaPaletteColor};
+
+#[derive(Default)]
+pub struct ForthCon {
+    fm: Arc<RwLock<ForthMachine>>
+}
 
 impl InstallableApp for ForthCon {
     fn install() -> (Path, Box<dyn AppConstructor>) {
-        (Path::from("forth"), Box::new(ForthCon)) 
+        (Path::from("forth"), Box::new(ForthCon::default())) 
     }
 }
-pub struct ForthCon;
 impl AppConstructor for ForthCon {
     fn instantiate(&self) -> Box<dyn LittleManApp> {
-        Box::new(ForthApp(Vec::new()))
+        Box::new(ForthApp(Vec::new(),self.fm.clone()))
     }
 }
 
-pub struct ForthApp(Vec<String>);
+pub struct ForthApp(Vec<String>, Arc<RwLock<ForthMachine>>);
 
 impl LittleManApp for ForthApp {
     fn start(&mut self, args: &[&str]) -> Result<(), StartError> {
@@ -26,16 +34,15 @@ impl LittleManApp for ForthApp {
         Ok(())
     }
     fn update(&mut self, handle: &mut OsHandle) {
-        if let Ok(_) = handle.text_mode_formatter() {
-            let mut fm = ForthMachine::default();
+        if let Ok(g) = handle.text_mode_formatter() {
             let segments: Vec<&str> = self.0.iter().map(|s| s.as_str()).collect();
-            fm.run(&segments)
+            self.1.write().run(&segments, g);
         }
         handle.call_exit();
     }
 }
 
-type ForthFunction = &'static dyn Fn(&mut ForthMachine);
+type ForthFunction = &'static (dyn Fn(&mut ForthMachine, &mut DefaultVgaWriter) + Sync + Send + 'static);
 
 type Stack = Vec<StackItem>;
 pub enum StackItem {
@@ -49,7 +56,7 @@ pub struct ForthMachine {
     stack: Stack,
 }
 
-fn add(sm: &mut ForthMachine) {
+fn add(sm: &mut ForthMachine, formatter: &mut DefaultVgaWriter) {
     if sm.stack.len() < 2 {
         return;
     }
@@ -60,17 +67,56 @@ fn add(sm: &mut ForthMachine) {
     } 
 }
 
-fn print(sm: &mut ForthMachine) {
+
+fn sub(sm: &mut ForthMachine, formatter: &mut DefaultVgaWriter) {
+    if sm.stack.len() < 2 {
+        return;
+    }
+    let x = sm.stack.pop().unwrap();
+    let y = sm.stack.pop().unwrap();
+    if let (StackItem::Int(y), StackItem::Int(x)) = (x, y) {
+        sm.stack.push(StackItem::Int(x-y));
+    }
+}
+
+fn div(sm: &mut ForthMachine, formatter: &mut DefaultVgaWriter) {
+    if sm.stack.len() < 2 {
+        return;
+    }
+    let x = sm.stack.pop().unwrap();
+    let y = sm.stack.pop().unwrap();
+    if let (StackItem::Int(y), StackItem::Int(x)) = (x, y) {
+        if y == 0 {
+            formatter.write_str("Tried dividing by zero");
+            return;
+        }
+        sm.stack.push(StackItem::Int(x/y));
+    }
+}
+
+fn mul(sm: &mut ForthMachine, formatter: &mut DefaultVgaWriter) {
+    if sm.stack.len() < 2 {
+        return;
+    }
+    let x = sm.stack.pop().unwrap();
+    let y = sm.stack.pop().unwrap();
+    if let (StackItem::Int(y), StackItem::Int(x)) = (x, y) {
+        sm.stack.push(StackItem::Int(x*y));
+    }
+}
+
+
+fn print(sm: &mut ForthMachine, formatter: &mut DefaultVgaWriter) {
     if sm.stack.len() < 1 {
         return;
     }
 
     if let Some(s) = sm.stack.pop() {
-        sm.print(s);
+        sm.print(s, formatter);
     }
 }
 
-fn wp(sm: &mut ForthMachine) {
+fn wp(sm: &mut ForthMachine, formatter: &mut DefaultVgaWriter) {
     if sm.stack.len() >= 4 {
         let tmp = (sm.stack.pop().unwrap(), sm.stack.pop().unwrap(), sm.stack.pop().unwrap(), sm.stack.pop().unwrap());
         if let (StackItem::Int(b), StackItem::Int(g), StackItem::Int(r), StackItem::Int(x)) = tmp {
@@ -84,43 +130,53 @@ fn wp(sm: &mut ForthMachine) {
     }
 }
 
-impl ForthMachine {
-    pub fn print(&self, s: StackItem) {
-        match s {
-            StackItem::String(s) => unsafe {
-                STATIC_VGA_WRITER.write_str(&s);
-            },
-            StackItem::Int(i) => unsafe {
-                STATIC_VGA_WRITER.write_str(&format!("{}", i));
-            },
-        }
-    }
-
-    pub fn default() -> Self {
+impl Default for ForthMachine {
+    fn default() -> Self {
         let mut tmp = Self {
             words: BTreeMap::new(),
             implemented_words: BTreeMap::new(),
             stack: vec![],
         };
         tmp.implemented_words.insert("+", &add);
+        tmp.implemented_words.insert("-", &sub);
+        tmp.implemented_words.insert("/", &div);
+        tmp.implemented_words.insert("*", &mul);
         tmp.implemented_words.insert(",", &print);
         tmp.implemented_words.insert("WP", &wp);
+
         tmp
     }
+}
 
-    pub fn run(&mut self, s:&[&str]) {
-        for word in s {
-            //self.print(StackItem::String(String::from("z")));
+impl ForthMachine {
+    pub fn print(&self, s: StackItem, formatter: &mut DefaultVgaWriter) {
+        match s {
+            StackItem::String(s) => {
+                formatter.write_str(&s);},
+            StackItem::Int(i) => 
+                {formatter.write_str(&format!("{}", i));},
+        }
+    }
+
+    pub fn run(&mut self, s: &[&str], formatter: &mut DefaultVgaWriter) {
+        let mut i = 0;
+        let mut new_i = 0;
+        while new_i < s.len() {
+            i = new_i;
+            new_i += 1;
+
+            let word = s[i];
+
             if let Ok(x) = isize::from_str_radix(word, 10) {
                 self.stack.push(StackItem::Int(x));
                 continue;
             }
-            //self.print(StackItem::String(String::from("y")));
+
             if self.implemented_words.contains_key(word) {
                 // Run it
-                //self.print(StackItem::String(String::from("x")));
+
                 let f = *self.implemented_words.get(word).unwrap();
-                f(self);
+                f(self, formatter);
             }
         }
     }
